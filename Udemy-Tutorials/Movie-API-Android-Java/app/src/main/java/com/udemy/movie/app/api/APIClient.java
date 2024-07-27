8b0970c7a40d37aea22997fb41a2dc1c33c85f2f
nilsdudhat <nilsdudhat14@gmail.com>
2024-07-27 19:30:08 +0530

    minor

--- Udemy-Tutorials/Movie-API-Android-Java/app/src/main/java/com/udemy/movie/app/api/APIClient.java
+++ Udemy-Tutorials/Movie-API-Android-Java/app/src/main/java/com/udemy/movie/app/api/APIClient.java
@@ -6,10 +6,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class APIClient {
 
     private static Retrofit retrofit = null;
-    private static String BASE_URL = "https://api.themoviedb.org/3/";
 
     public static APIInterface getMovieAPIRest() {
         if (retrofit == null) {
+            String BASE_URL = "https://api.themoviedb.org/3/";
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
